2011db2ae451b0a2176ed512dacb1c4621aac02e
Vivek <vivek9142@gmail.com>
2023-06-15 13:16:55 +0530

    added Array DS

---
+++ Array/01.js
@@ -0,0 +1,19 @@
+const strings = ["a", "b", "c", "d"];
+const numbers = [1, 2, 3, 4, 5];
+// Variable array is somewhere in memory and the computer knows it.
+// When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
+
+//push
+strings.push("e"); // O(1)
+
+//pop
+strings.pop(); // O(1)
+strings.pop();
+
+//unshift
+strings.unshift("x"); // O(n)
+
+//splice
+strings.splice(2, 0, "alien"); // O(n)
+
+console.log(strings);
